31433e56ad4b0d91034c9ec14b6bd794dd41efc1
Tomislav Peric <toca91@gmail.com>
2018-12-05 10:13:24 +0100

    #LACV-24 Showing how scope affects variables within and outside functions

--- js-scope-variables/main.js
+++ js-scope-variables/main.js
@@ -1,1 +1,17 @@
-document.write('hello!');
+var firstHuman = 'Alice';
+
+function greetHuman() {
+    // secondHuman variable is only accessible within function
+    var secondHuman = 'Bob';
+
+    document.write(`Hello ${firstHuman}<br>`);
+    document.write(`Hello ${secondHuman}`);
+}
+
+document.write('Global scope:<br>');
+greetHuman();
+
+document.write('<br><br>Local scope:<br>');
+document.write(`Hello ${firstHuman}`);
+// Undefined
+document.write(`Hello ${secondHuman}`);
