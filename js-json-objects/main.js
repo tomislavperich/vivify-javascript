02109ed9c80e5af27d085677f092637b9dc8e56b
Tomislav Peric <toca91@gmail.com>
2018-12-05 11:27:20 +0100

    #LACV-27 Showing JSON objects and JSON arrays of objects

---
+++ js-json-objects/main.js
@@ -0,0 +1,47 @@
+// JSON object
+var myInfo = {
+    "name": "Tomislav",
+    "age": 21,
+    "city": "Novi Sad",
+    "car": false
+}
+
+// Array of JSON objects
+var mutants = {
+    "john": {
+        "age": 30,
+        "city": "Belgrade",
+        "body-parts": [
+            {
+                "name": "arm",
+                "count": 2,
+            },
+            {
+                "name": "leg",
+                "count": 4,
+            },
+            {
+                "name": "head",
+                "count": 0,
+            },
+        ]
+    },
+    "mark": {
+        "age": 23,
+        "city": "Dubai",
+        "body-parts": [
+            {
+                "name": "arm",
+                "count": 3,
+            },
+            {
+                "name": "leg",
+                "count": 2,
+            },
+            {
+                "name": "head",
+                "count": 1,
+            },
+        ]
+    }
+}
